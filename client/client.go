59bf5e59e671d2db5b152aa1cde2a89e5129027a
jack-san-145 <santhosh34848550@gmail.com>
2026-04-24 12:28:01 +0000

    Initialize new client creation

--- client/client.go
+++ client/client.go
@@ -1,36 +1,19 @@
-package main
+package client
 
 import (
-	"bufio"
 	"context"
-	"log"
 	"net"
 	"net/http"
 )
 
-func main() {
+func CreateClient() http.Client {
 	lxr_sock := "/var/run/lxr.sock"
 
-	cli := http.Client{
+	return http.Client{
 		Transport: &http.Transport{
-			DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
+			DialContext: func(ctx context.Context, _, _ string) (net.Conn, error) {
 				return net.Dial("unix", lxr_sock)
 			},
 		},
 	}
-
-	res, err := cli.Get("http://unix/ping")
-	if err != nil {
-		log.Println("Request Error: ", err)
-		return
-	}
-	res_reader := bufio.NewReader(res.Body)
-	content, err := res_reader.ReadString('\n')
-	if err != nil {
-		log.Println("Error : ", err)
-	} else {
-		log.Println("Response from server: ", content)
-	}
-	defer res.Body.Close()
-
 }
