8c27a35c0e1f7e82b79cd9a005a5110bcac28594
jack-san-145 <santhosh34848550@gmail.com>
2026-04-26 11:20:27 +0000

    Initialize models for container creation

---
+++ models/models.go
@@ -0,0 +1,14 @@
+package models
+
+import ()
+
+type NewContainer struct {
+	Image         string `json:"image_name"`
+	ContainerName string `json:"container_name"`
+}
+
+type CreationResponse struct {
+	IsCreated     bool   `json:"is_created"`
+	ContainerName string `json:"container_name"`
+	ContainerId   string `json:"container_id"`
+}
