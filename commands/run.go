4d1c9fffe4a05eee7e9ac008d05a1b7a388e5617
jack-san-145 <santhosh34848550@gmail.com>
2026-04-30 06:08:54 +0000

    create run cmd to run the spawned container

---
+++ commands/run.go
@@ -0,0 +1,23 @@
+package commands
+
+import (
+	"fmt"
+
+	"lxr-cli/client"
+	"lxr-cli/response"
+)
+
+func Run(container_name string) {
+
+	cli := client.CreateClient()
+
+	res, err := cli.Get("http://lxr/run?container_name=" + container_name)
+	content, err := response.GetContainerRunResponse(res)
+	if err != nil || !content.Active {
+		//fmt.Println("Container Run Error:", err)
+		fmt.Println("container not found")
+		return
+	}
+
+	fmt.Println(container_name, "Running...")
+}
