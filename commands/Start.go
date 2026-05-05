c233084eb719245a0eed46929c4848adc35a6de2
jack-san-145 <santhosh34848550@gmail.com>
2026-05-05 20:52:00 +0000

    Update respone parsing in Start

--- commands/Start.go
+++ commands/Start.go
@@ -11,13 +11,13 @@ func Start(container_name string) {
 
 	cli := client.CreateClient()
 
-	res, err := cli.Get("http://lxr/start?container_name=" + container_name)
-	content, err := response.GetContainerRunResponse(res)
-	if err != nil || !content.Active {
-		//fmt.Println("Container Run Error:", err)
-		fmt.Println("container not found")
+	res, _ := cli.Get("http://lxr/start?container_name=" + container_name)
+	response, err := response.GetStartResponse(res)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
 		return
 	}
 
-	fmt.Println(container_name, ": Started")
+	fmt.Println(container_name + " " + response)
 }
