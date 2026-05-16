a2542c18a2cdf8926807ba9e0f38999dd3bf54e2
jack-san-145 <santhosh34848550@gmail.com>
2026-05-16 12:29:26 +0000

    Integrate kill response in response

--- response/response.go
+++ response/response.go
@@ -102,3 +102,20 @@ func GetStopResponse(res *http.Response) (string, error) {
 	}
 	return "Container Stopped", nil
 }
+
+func GetKillResponse(res *http.Response) (string, error) {
+
+	defer res.Body.Close()
+
+	var response models.KillResponse
+
+	err := json.NewDecoder(res.Body).Decode(&response)
+	if err != nil {
+		return "", err
+	}
+
+	if response.Killed {
+		return "Container Killed Successfully", nil
+	}
+	return "Kill Failured", nil
+}
