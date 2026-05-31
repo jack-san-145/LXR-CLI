18107b1a2a5e3296fa07b5e3e5e64677ef649439
jack-san-145 <santhosh34848550@gmail.com>
2026-05-31 08:35:47 +0000

    fix: disable socket timeout for live stream connections

--- client/client.go
+++ client/client.go
@@ -4,14 +4,13 @@ import (
 	"context"
 	"net"
 	"net/http"
-	"time"
 )
 
 func CreateClient() http.Client {
 	lxr_sock := "/var/run/lxr.sock"
 
 	return http.Client{
-		Timeout: 30 * time.Second,
+		Timeout: 0,
 		Transport: &http.Transport{
 			DialContext: func(ctx context.Context, _, _ string) (net.Conn, error) {
 				return net.Dial("unix", lxr_sock)
